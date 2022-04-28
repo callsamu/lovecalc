7437ac7a349de5b4e539e059342a889aa1c65015
callsamu <samuelbree@gmail.com>
2022-04-28 20:02:02 -0300

    hash-calculator: change algorithm so as to hash the sum of the two hashed strings

--- pkg/core/hash-calculator.go
+++ pkg/core/hash-calculator.go
@@ -12,19 +12,10 @@ type HashCalculator struct {
 	Hash hash.Hash
 }
 
-// Distance between two points p and q in an arc
-func distance(p, q float64) float64 {
-	min := math.Min(p, q)
-	max := math.Max(p, q)
-
-	d1 := max - min
-	d2 := maxValue - max + min
-
-	return math.Min(d1, d2)
-}
-
 func (c *HashCalculator) hash(b []byte) uint32 {
-	digest := c.Hash.Sum(b)
+	c.Hash.Write(b)
+	digest := c.Hash.Sum(nil)
+	c.Hash.Reset()
 	return binary.LittleEndian.Uint32(digest)
 }
 
@@ -33,7 +24,7 @@ func (c *HashCalculator) Compute(first, second string) float64 {
 	hash1 := c.hash([]byte(first))
 	hash2 := c.hash([]byte(second))
 
-	bytes := []byte{0x0, 0x0, 0x0, 0x0}
+	bytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(bytes, hash1+hash2)
 	hash3 := c.hash(bytes)
 
