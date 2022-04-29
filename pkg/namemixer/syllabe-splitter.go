0962ddf38e8bdbefa5dfe9c8a41b9be8bd9fea20
callsamu <samuelbree@gmail.com>
2022-04-28 22:24:38 -0300

    namemixer: Create tested syllabe splitted function

---
+++ pkg/namemixer/syllabe-splitter.go
@@ -0,0 +1,60 @@
+package namemixer
+
+var vowels = map[rune]struct{}{
+	'a': {}, 'e': {}, 'i': {}, 'o': {}, 'u': {},
+	'A': {}, 'E': {}, 'I': {}, 'O': {}, 'U': {},
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func nextVowel(runes []rune, start int) (int, bool) {
+	for i := start; i < len(runes); i++ {
+		_, isVowel := vowels[runes[i]]
+		if isVowel {
+			return i, true
+		}
+	}
+
+	return 0, false
+}
+
+func SplitSyllabes(word string) []string {
+	syllabes := []string{}
+	runes := []rune(word)
+
+	start := 0
+	end := 0
+
+	_, hasVowels := nextVowel(runes, start)
+	if !hasVowels {
+		return []string{word}
+	}
+
+	for {
+		pos, vowelsLeft := nextVowel(runes, start)
+		if !vowelsLeft {
+			return syllabes
+		}
+
+		peek, hasNextVowel := nextVowel(runes, pos+1)
+		if peek > pos+2 {
+			end = pos + 2
+		} else {
+			if !hasNextVowel {
+				end = len(runes)
+			} else {
+				end = pos + 1
+			}
+		}
+
+		syllabe := word[start:end]
+		syllabes = append(syllabes, syllabe)
+
+		start = end
+	}
+}
