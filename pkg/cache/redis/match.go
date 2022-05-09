79741f95757a648d14b869d64d8af44d3841eb6e
callsamu <samuelbree@gmail.com>
2022-05-09 20:42:23 -0300

    cache/redis: implement Get method for MatchCache

--- pkg/cache/redis/match.go
+++ pkg/cache/redis/match.go
@@ -1,6 +1,11 @@
 package redis
 
 import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+
+	"github.com/callsamu/lovecalc/pkg/cache"
 	"github.com/callsamu/lovecalc/pkg/core"
 	"github.com/go-redis/redis/v8"
 )
@@ -16,7 +21,24 @@ func NewMatchCache(rdc redis.Client) *MatchCache {
 }
 
 func (mc *MatchCache) Get(c core.Couple) (*core.Match, error) {
-	return nil, nil
+	val, err := mc.Client.Get(nil, c.FirstName+c.SecondName).Bytes()
+	if err != nil {
+		switch {
+		case errors.Is(err, redis.Nil):
+			return nil, cache.ErrKeyNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	var match core.Match
+	b := bytes.NewReader(val)
+
+	if err = gob.NewDecoder(b).Decode(&match); err != nil {
+		return nil, err
+	}
+
+	return &match, nil
 }
 
 func (mc *MatchCache) Set(c core.Couple, m *core.Match) error {
