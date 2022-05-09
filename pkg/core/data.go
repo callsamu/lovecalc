6b1640c336a8e40ce98fd679eca9db2944ebb04f
callsamu <samuelbree@gmail.com>
2022-05-08 23:33:57 -0300

    core: move old interface.go contents to data.go

--- pkg/core/data.go
+++ pkg/core/data.go
@@ -1,8 +1,20 @@
 package core
 
+import (
+	"crypto/sha1"
+	"errors"
+	"hash/adler32"
+	"hash/fnv"
+)
+
+const (
+	HashWithFNV     = "HashWithFNV"
+	HashWithSHA1    = "HashWithSHA1"
+	HashWithAdler32 = "HashWithAdler32"
+)
+
 // Simple PODs for containing input and results
 // used for serialization
-
 type Couple struct {
 	FirstName  string
 	SecondName string
@@ -12,4 +24,22 @@ type Match struct {
 	Couple
 	CoupleName  string
 	Probability float64
+}
+
+// The Calculator generic interface
+type Calculator interface {
+	Compute(Couple) *Match
+}
+
+func NewCalculator(algorithm string) (Calculator, error) {
+	switch algorithm {
+	case HashWithFNV:
+		return &HashCalculator{Hash: fnv.New128()}, nil
+	case HashWithSHA1:
+		return &HashCalculator{Hash: sha1.New()}, nil
+	case HashWithAdler32:
+		return &HashCalculator{Hash: adler32.New()}, nil
+	default:
+		return nil, errors.New("Unrecognized algorithm selected")
+	}
 }
