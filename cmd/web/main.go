6bf22c040eeaa2917a2dbf3992a1a59aa7f9870f
callsamu <samuelbree@gmail.com>
2022-05-09 23:04:14 -0300

    web: inject MatchCache dependency on main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,7 +9,10 @@ import (
 	"os"
 	"time"
 
+	"github.com/callsamu/lovecalc/pkg/cache"
+	redisc "github.com/callsamu/lovecalc/pkg/cache/redis"
 	"github.com/callsamu/lovecalc/pkg/core"
+	"github.com/go-redis/redis/v8"
 )
 
 type application struct {
@@ -17,10 +20,21 @@ type application struct {
 	infoLog       *log.Logger
 	errorLog      *log.Logger
 	templateCache map[string]*template.Template
+	matchCache    cache.MatchCache
+}
+
+func newRedisClient(url string) (*redis.Client, error) {
+	opt, err := redis.ParseURL(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return redis.NewClient(opt), nil
 }
 
 func main() {
 	addr := flag.Int("addr", 4000, "Port address to listen on")
+	redisUrl := flag.String("redis_url", os.Getenv("REDIS_URL"), "Redis instance URL")
 	algorithm := flag.String("algorithm", core.HashWithSHA1, "Algorithm to be used by the calculator")
 	flag.Parse()
 
@@ -31,6 +45,12 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	rdb, err := newRedisClient(*redisUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	matchCache := redisc.NewMatchCache(rdb)
 
 	templateCache, err := newTemplateCache("./ui/template/")
 	fmt.Println(templateCache)
@@ -43,6 +63,7 @@ func main() {
 		infoLog:       infoLog,
 		errorLog:      errorLog,
 		templateCache: templateCache,
+		matchCache:    matchCache,
 	}
 
 	srv := &http.Server{
