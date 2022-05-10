ce81aa3231a2fb7e86d3ae74190f65227b90487a
callsamu <samuelbree@gmail.com>
2022-05-09 23:10:47 -0300

    web: use cache on handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/callsamu/lovecalc/pkg/cache"
 	"github.com/callsamu/lovecalc/pkg/core"
 )
 
@@ -18,14 +20,33 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) results(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 
-	couple := core.Couple{query.Get("first"), query.Get("second")}
+	couple := core.Couple{
+		FirstName:  query.Get("first"),
+		SecondName: query.Get("second"),
+	}
 
 	if couple.FirstName == "" || couple.SecondName == "" {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
+	match, err := app.matchCache.Get(couple)
+	if err != nil {
+		switch {
+		case errors.Is(err, cache.ErrKeyNotFound):
+			match = app.calculator.Compute(couple)
+			err := app.matchCache.Set(couple, match)
+			if err != nil {
+				app.serverError(w, err)
+				return
+			}
+		default:
+			app.serverError(w, err)
+			return
+		}
+	}
+
 	app.render(w, r, "results.page.tmpl", &templateData{
-		Match: app.calculator.Compute(couple),
+		Match: match,
 	})
 }
