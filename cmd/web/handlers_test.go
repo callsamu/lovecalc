53cd997ea435b03b5e73e66ec9ae4c56c4e2f1d1
callsamu <samuelbree@gmail.com>
2022-05-14 18:48:19 -0300

    web: refactor result handler test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -22,35 +22,49 @@ func TestHome(t *testing.T) {
 	}
 }
 
+func TestResultsE2E(t *testing.T) {
+	app := newTestApplication(t)
+
+	srv := newTestServer(t, app.routes())
+	match := app.calculator.Compute(core.Couple{
+		FirstName:  "ha",
+		SecondName: "he",
+	})
+	prob := fmt.Sprintf("%d", int(toRoundedPercentage(match.Probability)))
+
+	status, _, body := srv.get(t, "/results?first=ha&second=he")
+
+	if status != http.StatusOK {
+		t.Errorf("expected status 200 OK, but got %d", status)
+	}
+
+	if !bytes.Contains(body, []byte(prob)) {
+		t.Errorf("expected body to contain substring %s", prob)
+	}
+}
+
 func TestResults(t *testing.T) {
 	app := newTestApplication(t)
 
-	t.Run("test sample computation", func(t *testing.T) {
-		srv := newTestServer(t, app.routes())
-		match := app.calculator.Compute(core.Couple{
-			FirstName:  "ha",
-			SecondName: "he",
+	cases := []struct {
+		name       string
+		first      string
+		second     string
+		wantStatus int
+	}{
+		{"test not supplied parameters", "", "", http.StatusSeeOther},
+	}
+
+	for _, ts := range cases {
+		t.Run(ts.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			url := fmt.Sprintf("/results?first=%s&second=%s", ts.first, ts.second)
+			req, _ := http.NewRequest("GET", url, nil)
+			app.results(rr, req)
+
+			if rr.Code != ts.wantStatus {
+				t.Errorf("expected status %d, but got %d", ts.wantStatus, rr.Code)
+			}
 		})
-		prob := fmt.Sprintf("%d", int(toRoundedPercentage(match.Probability)))
-
-		status, _, body := srv.get(t, "/results?first=ha&second=he")
-
-		if status != http.StatusOK {
-			t.Errorf("expected status 200 OK, but got %d", status)
-		}
-
-		if !bytes.Contains(body, []byte(prob)) {
-			t.Errorf("expected body to contain substring %s", prob)
-		}
-	})
-
-	t.Run("test not supplied parameters", func(t *testing.T) {
-		rr := httptest.NewRecorder()
-		req, _ := http.NewRequest("GET", "/results", nil)
-		app.results(rr, req)
-
-		if rr.Code != http.StatusSeeOther {
-			t.Errorf("expected status %d, but got %d", http.StatusSeeOther, rr.Code)
-		}
-	})
+	}
 }
