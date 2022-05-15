dc0f4d1b52fd44260d42e7a37737892e7b7d0c8c
callsamu <samuelbree@gmail.com>
2022-05-15 18:07:17 -0300

    pkg: implement translations package
    translations package embeds the locales directory
    which will contain the translations files, and can
    load it to a Bundle via the Load function

---
+++ pkg/translations/translations.go
@@ -0,0 +1,39 @@
+package translations
+
+import (
+	"embed"
+	"io/fs"
+
+	"github.com/BurntSushi/toml"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+// go:embed locales
+var LocalesFS embed.FS
+
+func Load(lfs fs.FS, defaultLang language.Tag) (*i18n.Bundle, error) {
+	bundle := i18n.NewBundle(defaultLang)
+	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+
+	err := fs.WalkDir(lfs, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+
+		mf, err := bundle.LoadMessageFileFS(lfs, d.Name())
+		if err != nil {
+			return err
+		}
+
+		return bundle.AddMessages(mf.Tag, mf.Messages...)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return bundle, nil
+}
---
+++ pkg/translations/translations_test.go
@@ -0,0 +1,41 @@
+package translations
+
+import (
+	"io/fs"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	"golang.org/x/text/language"
+)
+
+var mockTranslation = []byte(`
+[foo]
+description = "foo"
+other = "bazr"
+`)
+
+func TestTranslationLoadingE2E(t *testing.T) {
+	fs := fstest.MapFS{
+		"mock.en.toml": &fstest.MapFile{
+			Data:    mockTranslation,
+			Mode:    fs.ModeAppend,
+			ModTime: time.Now(),
+			Sys:     nil,
+		},
+	}
+
+	bundle, err := Load(fs, language.English)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	tags := bundle.LanguageTags()
+	if len(tags) != 1 {
+		t.Error("expected bundle to contain exactly one language tag")
+	}
+	if tags[0] != language.English {
+		t.Error("expected bundle to english language tag")
+	}
+}
