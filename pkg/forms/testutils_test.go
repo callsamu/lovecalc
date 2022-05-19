86dd84fb8b345c684650a7099fa399fb46507d69
callsamu <samuelbree@gmail.com>
2022-05-19 20:01:32 -0300

    forms: create helper function to set up message bundle

---
+++ pkg/forms/testutils_test.go
@@ -0,0 +1,37 @@
+package forms
+
+import (
+	"io/fs"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	"github.com/callsamu/lovecalc/pkg/translations"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+const errorMessages = `
+	[FooError]
+	other = "foo"
+	[BarError]
+	other = "bar"
+`
+
+func errMsgBundle(t *testing.T) *i18n.Bundle {
+	fs := fstest.MapFS{
+		"mock.en.toml": &fstest.MapFile{
+			Data:    []byte(errorMessages),
+			Mode:    fs.ModeType,
+			ModTime: time.Now(),
+			Sys:     nil,
+		},
+	}
+
+	bundle, err := translations.Load(fs, language.English)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return bundle
+}
