7dad2ebd6169fcac1d096db9c71c14fa298944a5
callsamu <samuelbree@gmail.com>
2022-05-19 17:20:27 -0300

    web: refactor template locale handling
    Create a field for injecting a localizer into a templateData
    struct and implement a shorthand method for using it on templates.
    Ditch the dynamic template map in the process.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -7,16 +7,25 @@ import (
 
 	"github.com/callsamu/lovecalc/pkg/core"
 	"github.com/callsamu/lovecalc/pkg/forms"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
 )
 
 type templateData struct {
-	Lang  string
-	Match *core.Match
-	Form  *forms.Form
+	Lang      string
+	Match     *core.Match
+	Form      *forms.Form
+	Localizer *i18n.Localizer
 }
 
 func toRoundedPercentage(x float64) float64 {
 	return math.Round(10000*x) / 100
+}
+
+func (td *templateData) translate(key string) (string, error) {
+	return td.Localizer.Localize(&i18n.LocalizeConfig{
+		MessageID:    key,
+		TemplateData: td,
+	})
 }
 
 var functions = template.FuncMap{
@@ -31,13 +40,10 @@ func newTemplateCache(dir string, lm *LocaleManager) (map[string]*template.Templ
 		return nil, err
 	}
 
-	funcs := functions
-	funcs["t"] = lm.localize // Set translator function
-
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(funcs).ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
