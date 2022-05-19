7bf801e853165f6a307e8d8d48c67d24828867e2
callsamu <samuelbree@gmail.com>
2022-05-19 20:41:44 -0300

    forms: implement i18n for error messages
    Make errors a struct with a localizer and configs to localizer
    calls. Create methods for messages with plurals and checking
    length.

--- pkg/forms/errors.go
+++ pkg/forms/errors.go
@@ -1,15 +1,38 @@
 package forms
 
-type errors map[string][]string
+import "github.com/nicksnyder/go-i18n/v2/i18n"
 
-func (e errors) Get(field string) string {
-	es := e[field]
-	if len(es) == 0 {
-		return ""
-	}
-	return es[0]
+type errors struct {
+	localizer *i18n.Localizer
+	configs   map[string][]*i18n.LocalizeConfig
 }
 
-func (e errors) Add(field, message string) {
-	e[field] = append(e[field], message)
+func (e *errors) Get(field string) (string, error) {
+	es := e.configs[field]
+	if len(es) == 0 {
+		return "", nil
+	}
+
+	return e.localizer.Localize(es[0])
+}
+
+// Use message with plural
+func (e *errors) Addc(field, message string, count int) {
+	cfg := &i18n.LocalizeConfig{
+		MessageID:   message,
+		PluralCount: count,
+		TemplateData: map[string]interface{}{
+			"count": count,
+		},
+	}
+	e.configs[field] = append(e.configs[field], cfg)
+}
+
+func (e *errors) Add(field, message string) {
+	cfg := &i18n.LocalizeConfig{MessageID: message}
+	e.configs[field] = append(e.configs[field], cfg)
+}
+
+func (e *errors) len() int {
+	return len(e.configs)
 }
